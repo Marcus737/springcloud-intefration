b5bec4ffbca9ba5e7f1d83328648faed5e031f88
Marcus737 <1040186242@qq,com>
2022-08-25 08:41:02 +0800

    11123

--- dept-service-swagger/src/main/java/wei/controller/DeptController.java
+++ dept-service-swagger/src/main/java/wei/controller/DeptController.java
@@ -1,10 +1,11 @@
-package com.wei.controller;
+package wei.controller;
 
-import com.wei.dao.DeptDao;
 import com.wei.dto.DeptDTO;
 import com.wei.service.DeptService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
-
 
 import javax.annotation.Resource;
 
@@ -16,10 +17,15 @@ public class DeptController {
     DeptService deptService;
 
     @PostMapping("/add")
+    @ApiOperation(value = "增加部门", notes = "增加部门数据")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "dept", value = "部门实体", required = true, dataType = "DeptDTO")
+    })
     public Object add(@RequestBody DeptDTO deptDTO){
         return deptService.add(deptDTO);
     }
 
+    @ApiOperation(value = "获取一个部门", notes = "根据部门编号获取其信息")
     @GetMapping("/get/{deptno}")
     public Object get(@PathVariable("deptno")long deptno){
         return deptService.get(deptno);
@@ -27,6 +33,7 @@ public class DeptController {
 
 
     @GetMapping("/list")
+    @ApiOperation(value = "部门信息列表", notes = "获取部门信息列表")
     public Object list(){
         return deptService.list();
     }
---
+++ dept-service-swagger/src/main/java/wei/po/Dept.java
@@ -0,0 +1,40 @@
+package wei.po;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Dept implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long deptno;
+    private String dname;
+    private String loc;
+
+    public Long getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Long deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getDname() {
+        return dname;
+    }
+
+    public void setDname(String dname) {
+        this.dname = dname;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+}
